6cec3f8464dfd0f37b22eb8233cf2854d6dd8530
freeman989 <62750259+freeman989@users.noreply.github.com>
2021-08-15 15:35:25 +0600

    Add files via upload

---
+++ ConsoleTimeTest/ConsoleTimeTest/ConsoleTimeTest.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <thread>
+#include <string>
+
+using namespace std;
+
+
+int main()
+{
+	int p = 1;
+	for (int i = 1; i <= 10; i++)
+	{
+		this_thread::sleep_for(chrono::seconds(1));
+
+		cout <<"int = " + to_string(p) << endl;
+
+		p++;
+	}
+
+	return 0;
+}
